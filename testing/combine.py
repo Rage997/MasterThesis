2abbaf6c5aabdc48fb4d8b09650960f962baa746
Rage997 <zuppin@usi.ch>
2022-02-07 10:30:33 +0100

    Testing folder

---
+++ testing/combine.py
@@ -0,0 +1,50 @@
+import math
+import stl
+from stl import mesh
+import numpy
+
+def find_mins_maxs(obj):
+    minx = obj.x.min()
+    maxx = obj.x.max()
+    miny = obj.y.min()
+    maxy = obj.y.max()
+    minz = obj.z.min()
+    maxz = obj.z.max()
+    return minx, maxx, miny, maxy, minz, maxz
+
+
+
+def translate(_solid, step, padding, multiplier, axis):
+    if 'x' == axis:
+        items = 0, 3, 6
+    elif 'y' == axis:
+        items = 1, 4, 7
+    elif 'z' == axis:
+        items = 2, 5, 8
+    else:
+        raise RuntimeError('Unknown axis %r, expected x, y or z' % axis)
+
+    # _solid.points.shape == [:, ((x, y, z), (x, y, z), (x, y, z))]
+    _solid.points[:, items] += (step * multiplier) + (padding * multiplier)
+
+
+def copy_obj(obj, dims, num_rows, num_cols, num_layers):
+    w, l, h = dims
+    copies = []
+    for layer in range(num_layers):
+        for row in range(num_rows):
+            for col in range(num_cols):
+                # skip the position where original being copied is
+                if row == 0 and col == 0 and layer == 0:
+                    continue
+                _copy = mesh.Mesh(obj.data.copy())
+                # pad the space between objects by 10% of the dimension being
+                # translated
+                if col != 0:
+                    translate(_copy, w, 0, col, 'x')
+                if row != 0:
+                    translate(_copy, l, 0, row, 'y')
+                if layer != 0:
+                    translate(_copy, h, 0, layer, 'z')
+                copies.append(_copy)
+    return copies
