1ef0798a63815cc5efe03d7bc95e5ee595f93662
Rage997 <zuppin@usi.ch>
2022-04-01 08:37:19 +0200

    Refactor

--- code/Rcode/EKF.R
+++ code/Rcode/EKF.R
@@ -1,68 +1,12 @@
-res=NULL
 require(Matrix)
 require(mgcv)
 
-# path = "~/Desktop/state space/Patent Data/results/"
-
-# n.iter = 500
-# d = 3
-# p = 5
-# p.y = p*(p-1)/2
-# n = 100
-# B.mc = 500
-# d.true = d
-# dd=1
-
-
-
-
-# network.type = "directed" # "undirected"
-# 
-# 
-# Y.gam = matrix( NA, n, p*(p-1) ) 
-# sender.gam = matrix( NA, n, p*(p-1) )
-# receiver.gam = matrix( NA, n, p*(p-1) )
-# time.gam = matrix( NA, n, p*(p-1) )
-# 
-# Y.kf = matrix( NA, n, p*(p-1)/2 )
-# 
-# for(t in 1:n){
-#   ii = 1  
-#   for(i in 1:(p-1))for(j in (i+1):p){
-#     Y.kf[t, ii] = Y.array[ i, j, t] + Y.array[ j, i, t]
-#     
-#     Y.gam[t, ii] = Y.array[ i, j, t]
-#     sender.gam[t, ii] = i
-#     receiver.gam[t, ii] = j
-#     time.gam[t, ii] = t
-#     #here you can add additional covariates
-#     ii = ii + 1
-#   }
-#   for(j in 1:(p-1))for(i in (j+1):p){
-#     Y.gam[t, ii] = Y.array[ i, j, t]
-#     sender.gam[t, ii] = i
-#     receiver.gam[t, ii] = j
-#     time.gam[t, ii] = t
-#     #here you can add additional covariates
-#     ii = ii + 1
-#   }
-# }
-# 
-# data.gam = as.data.frame(cbind(as.vector(t(Y.gam)), as.vector(t(sender.gam)), as.vector(t(receiver.gam)), as.vector(t(time.gam)), NA ) )
-# names(data.gam) = c("Y", "sender", "receiver", "time", "E")
-# data.gam$sender = as.factor(data.gam$sender)
-# data.gam$receiver = as.factor(data.gam$receiver)
-# 
-# 
-# 
-# 
-# 
-# 
-# formula.lin.pred = if(network.type=="directed"){
-#   formula(Y ~ offset(log(E)) )#+ s(time) + s(sender, bs="re") + s(receiver, bs="re")) 
-# }else{
-#   formula(Y ~ offset(log(2) + log(E)) ) #+ s(time) + s(sender, bs="re") + s(receiver, bs="re")) 
-# }
+# Taken from SimLatentSpace
+n.iter = 500
+d = 3
+p = 5
+p.y = p*(p-1)/2
+n = 100
 
 
 # hazard function / rate -> poisson variable
@@ -74,19 +18,16 @@ d.h.function = function(x.i, x.j, gam.sum.pred.ij){
 }
 
 # Inizialiation T=0
-# H=diag(p)
 X.prior = X.post = matrix(NA, n, p*d) # points in latent space (Expected value of X)
 P.prior = P.post = array(NA, dim=c(p*d,p*d,n)) # variance of X
 
 #tuning parameters
-# cc = 12
 Q = diag(0.001, p*d) # X_t+1  = X_T + eps_t : sigma is var(eps)
-# R = diag(0.01^2, p.y)
-
 
 # Inizialization: starting points    TRY WITH DIFFERENT X STARTING POINTS!
 P_0.smooth = P_0 = Q
 #x_0.smooth = x_0 = runif(p*d, -1, 1)
+# TODO is this X taken from SimLatentSpace?
 x_0.smooth = x_0 = as.vector(matrix( X[ 1, ], nrow=d.true)[1:d, ])
 
 # Populate H_ij = [derivative function(i,j)] -> sparse matrix
@@ -99,8 +40,6 @@ build.H = function(x.prior, gam.sum.pred.vec){
   }
   return(H)
 }
-
-
 
 
 hess = function(x.i, x.j){
@@ -133,51 +72,10 @@ hess = function(x.i, x.j){
   res
 }
 
-
-# hess2 = function(x1, x2){
-#   if( d>3 | d<1 )stop("the hessian is implemented for dimension 2 and 3 only")
-#   dist.tmp=dist(rbind(x1, x2))
-#   res = matrix(NA, 2*d, 2*d)
-#   
-#   diag(res)[1:d] = diag(res)[(d+1):(2*d)] = ( (x1-x2)^2  - dist.tmp^2) /(dist.tmp^3)
-#   
-#   if(d==2){
-#     res[1, 3] = res[3, 1] = - res[1, 1]
-#     res[2, 4] = res[4, 2] = - res[2, 2]
-#     
-#     res[1, 4] = res[4, 1] = res[2, 3] = res[3, 2] = -(x1-x2)[1]*(x1-x2)[2]/(dist.tmp^3)
-#     res[1, 2] = res[2, 1] = res[3, 4] = res[4, 3] = - res[1, 4]
-#   }else if(d==3){
-#     res[1, 4] = res[4, 1] = - res[1, 1]
-#     res[2, 5] = res[5, 2] = - res[2, 2]
-#     res[3, 6] = res[6, 3] = - res[3, 3]
-#     
-#     res[1, 5] = res[5, 1] = res[2, 4] = res[4, 2] = -(x1-x2)[1]*(x1-x2)[2]/(dist.tmp^3)
-#     res[1, 6] = res[6, 1] = res[3, 4] = res[4, 3] = -(x1-x2)[1]*(x1-x2)[3]/(dist.tmp^3)
-#     res[2, 6] = res[6, 2] = res[3, 5] = res[5, 3] = -(x1-x2)[2]*(x1-x2)[3]/(dist.tmp^3)
-#     res[1, 2] = res[2, 1] = res[4, 5] = res[5, 4] = - res[1, 5]
-#     res[1, 3] = res[3, 1] = res[4, 6] = res[6, 4] = - res[1, 6]
-#     res[2, 3] = res[3, 2] = res[5, 6] = res[6, 5] = - res[2, 6]
-#   }
-#   res
-# }
-
-# scale.data = T
-# 
-# if(scale.data==T){res.fact = 1/pmax(apply(Y.kf, 2, max), 1)}else{res.fact = rep(1, ncol(Y.kf))} #rescale all varaibles in [0,1]
-# gam.sum.pred.mat = gam.sum.pred.mat %*% diag(res.fact) #rescale columns
-# Y.kf = Y.kf %*% diag(res.fact) #rescale columns
-
-# Y.kf = matrix(pmax( as.vector(Y.kf), 1), nrow=n)
-
-#x_0 = 0.5*x_0
-
 repeat.filter=1
-
 hist.list = NULL
 gam.save = NULL
 
-#system.time(
 
 for(iter in 1:n.iter){
   for(t in 1:n){ #Kalmann Filter
@@ -196,17 +94,9 @@ for(iter in 1:n.iter){
       # vv = res.fact * vv
       R = diag(vv)
       R.inv = diag(1/vv)
-      # K = P.prior[ , , t] %*% t(H) %*% solve( H %*% P.prior[ , , t] %*% t(H) + R )
-      # X.post[ t, ] = X.prior[ t, ] + K %*% ( Y[ t, ] - h.function(X.prior[ t, ]) )
-      # P.post[ , , t] = (diag(p*d) - K %*% H) %*% P.prior[ , , t]
-      # tryCatch(
-      #   K <<- crossprod( t(tcrossprod( P.prior[ , , t], H)), solve( tcrossprod(crossprod(t(H), P.prior[ , , t]), H) + R, tol=1e-70) ),
-      #   error = function(e) K <<- crossprod( t(tcrossprod( P.prior[ , , t], H)), solve( off + tcrossprod(crossprod(t(H), P.prior[ , , t]), H) + R, tol=1e-70) )
-      # )
       # Sherman-Morrison-Woodbury identity (page 31) -> kinda a lie but okay
       K = tcrossprod(solve(solve(P.prior[ , , t], tol = 1e-70) + crossprod(H, crossprod(R.inv, H)), tol = 1e-70),  t( crossprod(H, R.inv)) )
       
-      # K = crossprod( t(tcrossprod( P.prior[ , , t], H)), solve( tcrossprod(crossprod(t(H), P.prior[ , , t]), H) + R, tol=1e-70) )
       X.post[ t, ] = X.prior[ t, ] + crossprod(t(K), ( Y.kf[ t, ] - mu )) # see algo 1 pg 10: expected value
       vv = h.function(X.post[ t, ], gam.sum.pred.mat[ t, ]) # useless
     }
@@ -262,56 +152,10 @@ for(iter in 1:n.iter){
   }
   
   Q.hat = Q.hat/n
-  Q = diag(rep(apply(matrix(diag(Q.hat), ncol=p), 2, mean), each=d))
-
-  
-  # E1 = matrix( NA, n, p*(p-1)/2 ) 
-  # # E2 = matrix( NA, n, p*(p-1)/2 ) 
-  # for(t in 1:n){
-  #   ii=1
-  #   for(i in 1:(p-1)){
-  #     for(j in (i+1):p){
-  #       i.ind = (i-1)*d + (1:d)
-  #       j.ind = (j-1)*d + (1:d)
-  #       x.i = X.smooth[t, i.ind]
-  #       x.j = X.smooth[t, j.ind]
-  #       E1[t, ii] = exp(-dist(rbind(x.i, x.j))^2) + 0.5 * sum(diag(hess(x.i, x.j) %*% P.smooth[ c(i.ind, j.ind), c(i.ind, j.ind), t]) )
-  #       # E2[ii, t] = -dist(rbind(x.i, x.j)) + 0.5 * sum(diag(hess2(x.i, x.j) %*% P.smooth[ c(i.ind, j.ind), c(i.ind, j.ind), t]) )
-  #       if(E1[t, ii]<0 | E1[t, ii]>1){
-  #         print(paste("Nodes too close at iteration ", iter, ", time, ", t,", at pair (", i,", ", j, ")", sep = ""))
-  #         E1[t, ii] = exp(-dist(rbind(x.i, x.j))^2)
-  #         print(E1[t, ii])
-  #         if(E1[t, ii] == 0) print(paste("Error: infinite distance at iteration ", iter, ", time, ", t,", at pair (", i,", ", j, ")", sep = ""))
-  #       }
-  #       ii = ii + 1
-  #     }
-  #   }
-  # }
-  # # #cc = log(sum(Y)) - log(sum(E1))
-  # # # E.llik[iter] = -n*log(det.Q)/2 - sum( E1 * exp(cc) ) + sum( as.vector(t(Y)) * (as.vector(t(E2)) + cc)  )
-  # # #CHECK SE HESSIANO E QUESTA FORMULA SONO CORRETTI
-  #
-  #
-  # data.gam$E = as.vector(t(cbind(E1, E1)))
-  # SEE: https://m-clark.github.io/
-  # fit.gam = gam( formula.lin.pred, family = poisson(link = "log"), data = data.gam )
-  # gam.sum.pred.mat = matrix( predict( fit.gam, type="response" ), nrow = n, ncol = p*(p-1), byrow=T )
-  # 
-  # gam.sum.pred.mat = if(network.type=="directed"){ gam.sum.pred.mat/cbind(E1, E1) }else{ gam.sum.pred.mat/(2*cbind(E1, E1)) }
-  # gam.sum.pred.mat = gam.sum.pred.mat[ , 1:(p*(p-1)/2)] + gam.sum.pred.mat[ , (p*(p-1)/2 + 1):(p*(p-1))]
-  # 
-  # gam.save = c(gam.save, list(fit.gam$coefficients))
-  
-
-  # cat(floor(100*iter/n.iter), "%" , " \r")
-  # flush.console()
-  
+  Q = diag(rep(apply(matrix(diag(Q.hat), ncol=p), 2, mean), each=d))  
 }
 
-#) -> compTime #system.time end
-
 #empirical Kullback Leibler
-
 E1 = matrix( NA, n, p*(p-1)/2 )
 # E2 = matrix( NA, n, p*(p-1)/2 )
 for(t in 1:n){
@@ -335,11 +179,6 @@ for(t in 1:n){
   }
 }
 
-#re-expand data
-# if(scale.data==T){
-# gam.sum.pred.mat = gam.sum.pred.mat %*% diag(1/res.fact) #rescale columns
-# Y.kf = Y.kf %*% diag(1/res.fact) #rescale columns
-# }
 
 KL=NULL
 for(k in 1:100){
@@ -347,12 +186,3 @@ for(k in 1:100){
   KL= c(KL, mean( dpois(Y.gen, lambda.true.sum, log=T) - dpois(Y.gen, E1*gam.sum.pred.mat, log=T)  ) )
 }
 KL=mean(KL)
-
-# ref = fit.gam$coefficients[(length(fit.gam$coefficients)-2*p+1):length(fit.gam$coefficients)] - c(sender.true - mean(sender.true), receiver.true - mean(receiver.true)) 
-# ref = sqrt(sum(rss^2)/(2*p))
-
-
-#save(KL, compTime, file=paste("/home/artico/simulation1/EKF_simId", bbb,"_n", n,"_p", p,"_d", d,"_dTrue", d.true,"_dd", dd,".RData", sep="") )
-
-res = c( KL, as.numeric(compTime[3]) )
-
--- code/Rcode/SimLatentSpace.R
+++ code/Rcode/SimLatentSpace.R
@@ -1,37 +1,33 @@
-
-#uncomment following 15 lines
-# require(Matrix)
-# require(mvtnorm)
-
+# Params
 negBin=F
 alpha = 3
 # n.iter = 10
 p = 10
 p.y = p*(p-1)/2
 n = 100
+# TODO d, and d.true are the same?
 d = 2
 d.true = d
 dd=1
 n.clust = 3
 
-
-
 time = 1:n
-# sender.true = rnorm(p, 0, 0.5)
-# receiver.true = rnorm(p, 0, 0.5)
-# spline.time.true = 3 + c(-1, 1)[rbinom(1, 1, 0.5) + 1] * (exp(10*time/n - 5)/(1 + exp(10*time/n - 5))-.5)
 
+# Init vector with 0
 sender.true = rep(0, p)
 receiver.true = rep(0, p)
 spline.time.true = rep(alpha, n)
 
-
-
+# TODO: what is this?
 logis=function(time, a, b, c) a*(exp(10*(time/n-b))/(1 + exp(10*(time/n-b)))-.5) + c
 
-# time=seq(0, 10, length.out = n)
+# Generate a, b, c from uniform distribution using diffrent parameters
+# In particular, c will separate the trajectories into clusters.
+
+# Uniform distribution ruinif(x, min, max)
 a = runif(p*d.true, -1, 1)*1
 b = rep(runif(p, 0.2, 0.8), d.true)
+# Clustering (for what means?)
 c.clust=0
 if(n.clust>1){
   print("clustering implemented for dimension 2 only")
@@ -39,16 +35,16 @@ if(n.clust>1){
   c.clust = as.vector(t(c.clust))
 }
 c = runif(p*d.true,-dd,dd) + c.clust
-# cc.true = cc = 3
+
+
 X=matrix(NA, n, p*d.true)
 
 for(i in 1:(p*d.true))X[, i] = logis(time, a[i], b[i], c[i]) 
-
-# for(i in 1:n)plot(matrix(X[i,], ncol=2, byrow=T ), main=i)
 # Plot trajectories 
 matplot(X, ty="l")
 
 
+# TODO: no idea? estimating lambda?
 lambda.true = array(NA, dim = c(p, p, n))
 Y.array = array(NA, dim = c(p, p, n))
 for(t in 1:n)for(i in 1:p)for(j in 1:p){
@@ -58,16 +54,15 @@ for(t in 1:n)for(i in 1:p)for(j in 1:p){
   }
 }
 
-plot(time, lambda.true[ 1, 2, ], ylim=c(0, max(Y.array, na.rm = T)), ty="l")
-# plot( time, Y.array[ 1, 2, ] )
+# plot(time, lambda.true[ 1, 2, ], ylim=c(0, max(Y.array, na.rm = T)), ty="l")
+
 for(i in 1:p)for(j in 1:p){
   if( i!=j ){
     lines(time, lambda.true[ i, j, ], col=i+j)
-    # points( time, Y.array[ i, j, ], ylim=c(0, max(Y, na.rm = T)) )
   }
 }
 
-
+# TODO: go over this again?
 Y.kf = matrix( NA, n, p*(p-1)/2 )
 lambda.true.sum = matrix(NA, n, p*(p-1)/2) 
 for(t in 1:n){
@@ -86,69 +81,3 @@ gam.sum.pred.mat = matrix( 2*exp(alpha), n, p*(p-1)/2)  # its twice the rate!!!!
 Y.kf.static = apply(Y.kf, 2, sum)
 Y.kf.static = matrix(Y.kf.static, nrow=1)
 gam.sum.pred.mat.static = matrix( n*2*exp(alpha), 1, p*(p-1)/2)  # its twice the rate!!!!!!!
-
-##########################################################################################################
-# n.time.points = n/4
-# D.hat = matrix(0, p, p)
-# D.hat[lower.tri(D.hat)] = sqrt( pmax(0, log(2) + 3 - log(apply(Y.kf[1:(n.time.points),] + 1e-30, 2, mean) )) )
-# D.hat = D.hat + t(D.hat)
-# tmp = cmdscale( D.hat, k=d, add=F )
-# # par(mfrow=c(1,2))
-# # plot(tmp, col=1:p)
-# # plot(matrix( X[ 1, ], ncol=d.true, byrow=T), col=1:p)
-# # plot(matrix( X[ 1, ], ncol=d.true, byrow=T)[ , 1],  tmp[, 1])
-# # plot(matrix( X[ 1, ], ncol=d.true, byrow=T)[ , 1],  tmp[, 1])
-# 
-# 
-# # plot(dist(matrix( X[ 1, ], ncol=d.true, byrow=T)), dist(tmp) )
-# # abline(0,1, col=2)
-# 
-# x_0  = as.vector(t(tmp))
-# # x_0 = runif(p*d, -20, 20)
-# 
-# 
-# llik = function(beta, x){
-#   lambda =  exp( beta - as.vector(dist(matrix(x, ncol=d, byrow=T))^2))
-#   sum(-lambda * n.time.points) + sum(Y.kf[1:(n.time.points),] %*% log(lambda))
-# }
-# 
-# B=100000
-# llik.save = -Inf
-# x.mcmc = matrix(NA, B, p*d)
-# beta.mcmc = rep(NA, B)
-# 
-# eps1 = 0.01
-# eps2 = 0.01
-# x.mcmc[1, ] = x_0
-# beta.mcmc[1] = log(2) + 3
-# 
-# 
-# #run simulated annealing mcmc
-#   
-#   for(b in 2:B){
-#     x.mcmc[b, ] = x.mcmc[b-1, ]
-#     beta.mcmc[b] = beta.mcmc[b-1]
-#     x.mcmc.proposed = x.mcmc[b-1, ] + rnorm( p*d, 0, eps1)
-#     beta.mcmc.proposed = beta.mcmc[b-1] + rnorm( 1, 0, eps2)
-#     
-#     llik.proposed = llik(beta.mcmc.proposed, x.mcmc.proposed)
-#     if(runif(1) < 1000/b *exp(llik.proposed - llik.save ) ){
-#       llik.save = llik.proposed
-#       x.mcmc[b, ] = x.mcmc.proposed
-#       beta.mcmc[b] = beta.mcmc.proposed
-#     }
-#   }
-# 
-# matplot(x.mcmc, ty="l")
-# matplot(beta.mcmc, ty="l")
-# 
-# x_0 = x.mcmc[B, ]
-# 
-# 
-# 
-
-
-
-
-
-
