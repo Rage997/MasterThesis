dba4dbec14cc8d644cb02d084f37474d54f205c7
Rage997 <zuppin@usi.ch>
2022-04-08 12:47:09 +0200

    Updated with Igor code

--- code/Rcode/EKF.R
+++ code/Rcode/EKF.R
@@ -36,25 +36,22 @@ Q = diag(0.001, p*d) # X_t+1  = X_T + eps_t : sigma is var(eps)
 P_0.smooth = P_0 = Q
 #x_0.smooth = x_0 = runif(p*d, -1, 1)
 # TODO generate it from a gaussian or whatever
-x_0.smooth = x_0 = rnorm(p*d, 0, 1)
+x_0.smooth = x_0 #= X[1, ]#rnorm(p*d, 0, 1)
   #as.vector(matrix( X[ 1, ], nrow=d.true)[1:d, ])
 
 # Populate H_ij = [derivative function(i,j)] -> sparse matrix
 build.H = function(x.prior, gam.sum.pred.vec){
   H = matrix(0, p.y, p*d)
   ii = 1
-  #for(i in 1:(p-1))for(j in (i+1):p){
-  for(i in 1:(p-1))for(j in (i+1):p){
-    # we are out of range here....can't figure out why?
+  for(i in 1:s)for(j in (s+1):p){
     H[ii, c(d*i - (d-1):0, d*j - (d-1):0)] = d.h.function(x.prior[c(d*i - (d-1):0)], x.prior[c(d*j - (d-1):0)] , gam.sum.pred.vec[ii] )  
     ii = ii + 1 
   }
   return(H)
 }
-repeat.filter=1
-hist.list = NULL
-gam.save = NULL
 
+zero.rate = function(y)apply(y, 2, function(x)!any(x>0))
+eps = 1e-5
 
 for(iter in 1:n.iter){
   for(t in 1:n){ #Kalmann Filter
@@ -69,17 +66,18 @@ for(iter in 1:n.iter){
     H = build.H(X.prior[ t, ], gam.sum.pred.mat[ t, ]) #first derivative (gam.sum is an offset) on X_prior
     # you can do taylor: mu(x) = mu(x0) + d_h(x - x0)
     mu = h.function(X.prior[ t, ], gam.sum.pred.mat[ t, ]) # poisson parameter -> variance
+    if(t>1 & apply.zero.rate){
+      mu = mu*zero.rate(matrix(Y.kf[1:(t-1), ], ncol=p.y))
+      H = H*zero.rate(matrix(Y.kf[1:(t-1), ], ncol=p.y))
+    }
     vv = mu 
-    #for(k in repeat.filter){ # not necessary
-      # vv = res.fact * vv
       R = diag(vv)
       R.inv = diag(1/vv)
+      R.inv[R.inv==Inf] = 0
       # Sherman-Morrison-Woodbury identity (page 31) -> kinda a lie but okay
       K = tcrossprod(solve(solve(P.prior[ , , t], tol = 1e-70) + crossprod(H, crossprod(R.inv, H)), tol = 1e-70),  t( crossprod(H, R.inv)) )
       
       X.post[ t, ] = X.prior[ t, ] + crossprod(t(K), ( Y.kf[ t, ] - mu )) # see algo 1 pg 10: expected value
-      vv = h.function(X.post[ t, ], gam.sum.pred.mat[ t, ]) # useless
-    #}
     
     P.post[ , , t] = crossprod(t(diag(p*d) - crossprod(t(K), H) ), P.prior[ , , t]) # see algo 1 pg 10: variance
     P.post[ , , t] = (P.post[ , , t] + t(P.post[ , , t]))/2
@@ -116,7 +114,7 @@ for(iter in 1:n.iter){
       }  
     }
     B.save[ , , t] = B 
-  } -> compTime #system.time end
+  }
   
   x_0 = x_0.smooth
   P_0 = P_0.smooth
@@ -132,7 +130,20 @@ for(iter in 1:n.iter){
   }
   
   Q.hat = Q.hat/n
-  Q = diag(rep(apply(matrix(diag(Q.hat), ncol=p), 2, mean), each=d))  
+  Q = diag(rep(apply(matrix(diag(Q.hat), ncol=p), 2, mean), each=d))
+
+    #fit additional parameters in linear predictor
+  D=Y.kf
+  for(t in 1:n){
+    x.vec = X.smooth[t, ]
+    D[t, ] = -as.vector(t(pointDistance(matrix(x.vec[1:(s*d)], ncol=d, byrow=T), matrix(x.vec[(s*d+1):((s+r)*d)], ncol=d, byrow=T), lonlat=F, allpairs = T )^2))
+    if(t>1 & apply.zero.rate){
+      D[t, !zero.rate(matrix(Y.kf[1:(t-1), ], ncol=p.y))] = NA
+    }
+  }
+
+  alpha.est = glm(as.vector(Y.kf[!is.na(D)]) ~ 1 + offset(as.vector(D[!is.na(D)])), family=poisson(link = "log"))$coef
+  gam.sum.pred.mat = matrix( exp(alpha.est), n, p.y )
 }
 
 
