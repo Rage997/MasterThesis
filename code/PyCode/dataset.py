6b85ae6f7c3a7ee50b1891ba2361a18719c3230e
Rage997 <zuppin@usi.ch>
2022-04-12 16:18:04 +0200

    Function to remove non-relevant nodes

--- code/PyCode/dataset.py
+++ code/PyCode/dataset.py
@@ -1,3 +1,4 @@
+from time import time
 import pandas as pd
 import numpy as np
 
@@ -6,7 +7,6 @@ This package contains function for handling the dataset, filtering
 with revelant data, reporting information about it and converting it
 to a matrix to be lately processed with a REM model
 '''
-
 class InvasiveSpecies:
     def __init__(self, filename: str) -> None:
        
@@ -22,22 +22,67 @@ class InvasiveSpecies:
         self.Ns = len(self.species)
         self.Nr = len(self.region)
 
-        t_min = self.df['FirstRecord'].min()
-        t_max = self.df['FirstRecord'].max()
-        self.time = np.arange(t_min, t_max, 2)
-
     def _read_data(self, filename: str)->pd.DataFrame:
         df_orig = pd.read_excel(filename, sheet_name=None)
         # There are 3 sheets of the excel file. Get the correct one
         df = df_orig['GlobalAlienSpeciesFirstRecordDa']
         return df
 
+    def get_time_interval(self):
+        t_min = self.df['FirstRecord'].min()
+        t_max = self.df['FirstRecord'].max()
+        return (t_min, t_max)
+
     def filter_data(self)->None:
-        self.df = self.df[self.df['LifeForm'] == 'Viruses']
+        # self.df = self.df[self.df['LifeForm'] == 'Viruses']
         self.df = self.df[self.df['FirstRecord'] > 1950]
         # return df
 
-    def species_info(df: pd.DataFrame)->None:
+    def remove_irrelevant(self, species_tol: int, region_tol: int):
+        '''Removes irrelevant nodes'''
+
+        df = self.df
+        spec_iter = np.zeros(len(self.species), dtype=int)
+        reg_iter = np.zeros(len(self.region), dtype=int)
+
+        for idx, s in enumerate(self.species):
+            df_s = df[df['TaxonName'] == s]
+            # n_iter = len(df_s)
+            spec_iter[idx] = len(df_s)
+            # print(f'The species {s} has {spec_iter[idx]} iteractions')
+
+        for idx, s in enumerate(self.region):
+            df_s = df[df['Region'] == s]
+            # n_iter = len(df_s)
+            reg_iter[idx] = len(df_s)
+            # print(f'The species {s} has {spec_iter[idx]} iteractions')
+
+        # select the species that have less than 3 iteractions
+        remove_s_idx = np.where(spec_iter < species_tol)
+        # and regions that are invaded by less than 10 species
+        remove_r_idx = np.where(reg_iter < region_tol)
+
+        # Filter the dataset: remove all rows belonging to species with less than 3 iteractions
+        for s in self.species[remove_s_idx]:
+            # print(s)  
+            df = df.drop(df[df['TaxonName'] == s].index)
+
+        # Filter the dataset: remove all rows belonging to region with less than 10 invasions
+        for s in self.region[remove_r_idx]:
+            # print(s)  
+            df = df.drop(df[df['Region'] == s].index)
+
+        species_removed = len(self.species) - len(df['TaxonName'].unique())
+        print(f'Removed {species_removed} species')
+        region_removed = len(self.region) - len(df['Region'].unique())
+        print(f'Removed {region_removed} region')
+        
+        # update class variables
+        self.region = df['Region'].unique()
+        self.species = df['TaxonName'].unique()
+        self.df = df
+
+    def log_species_info(df: pd.DataFrame)->None:
         family = df['LifeForm'].unique()
         region = df['Region'].unique()
 
@@ -50,16 +95,19 @@ class InvasiveSpecies:
             animals = df_s['TaxonName'].unique()
             print(f'{p}[{len(animals)}]', end=', ')
 
-    def build_matrix(self)->np.array:
+    def build_matrix(self, time_resolution:int=5)->np.array:
+
+        t_min, t_max = self.get_time_interval()
+        self.time = np.arange(t_min, t_max, time_resolution)
 
         # Non square matrix M \in R^{T, Ns, Nr}
         M = np.zeros((len(self.time), self.Ns, self.Nr))
         # or...build full matrix
         # p = self.Ns + self.Nr
-        # M = np.zeros((len(time), p, p)) # if full
-
+        # M = np.zeros((len(self.time), p, p)) # if full
+    
         for i, t in enumerate(self.time):
-            df_now = self.df[(self.df['FirstRecord'] >= t) & (self.df['FirstRecord'] < t+1)]
+            df_now = self.df[(self.df['FirstRecord'] >= t) & (self.df['FirstRecord'] < t+(time_resolution-1))]
             for _, row in df_now.iterrows():
                 s = row['TaxonName']
                 r = row['Region']
