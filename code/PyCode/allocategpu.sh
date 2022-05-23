12359871960c5d6a7fcd2a7a420b754d6e048e54
Rage997 <zuppin@usi.ch>
2022-05-23 17:59:29 +0200

    Script to allocated GPU job on cluster

--- code/PyCode/allocategpu.sh
+++ code/PyCode/allocategpu.sh
@@ -1,1 +1,4 @@
- salloc -J masterthesis -t 100 --partition=gpu
+salloc -J masterthesis -t 600 --partition=gpu
+module load gcc/10.1.0
+module load cudatoolkit
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
