a976e14a4daa067fc4a6048a8c1ae136b580abf0
Rage997 <zuppin@usi.ch>
2022-06-02 08:47:15 +0200

    Added model_particle code and save simulation results

--- code/PyCode/model_particle.py
+++ code/PyCode/model_particle.py
@@ -1,5 +1,4 @@
 # simulate a clustering dynamics process
-from re import S
 import tensorflow as tf
 import tensorflow_probability as tfp
 tfd = tfp.distributions
@@ -24,9 +23,10 @@ if gpus:
     # Memory growth must be set before GPUs have been initialized
     print(e)
 
+
 # -------------------- Load real data -------------------------
 # Load data from memory
-Y = np.load('../data/matrix_412_58.npy')
+Y = np.load('../data/matrix_730_123.npy')
 d = tf.constant(2, dtype =tf.int32)
 n = tf.constant(Y.shape[0], dtype =tf.int32)
 s = tf.constant(Y.shape[1], dtype =tf.int32) 
@@ -47,7 +47,14 @@ for i in range(n_nodes):
 
 X_true = tf.constant(X_true, dtype = tf.float32)
 
+
 # ------------------- Model declaration -------------
+
+def h_b(X_b, b, c):
+  species = tf.reshape(X_b[:, :(s*d)], (b, s, d))
+  region = tf.reshape(X_b[:, (s*d):], (b, r, d))
+  return tf.exp( c + tf.reshape(tf.matmul(species, region, transpose_b=True), (b, s*r)))
+
 def h(x, c):
   species = tf.reshape(x[:(s*d)], (s, d))
   region = tf.reshape(x[(s*d):], (r, d))
@@ -58,22 +65,13 @@ def get_offset(x):
   region = tf.reshape(x[(s*d):], (r, d))
   return tf.reshape(tf.matmul(species, region, transpose_b=True), [-1])
 
-
+@tf.function
 def get_h_H(x, c): 
-  with tf.GradientTape() as tape:
+  with tf.GradientTape(persistent=True) as tape:
     tape.watch(x)
     h_current = h(x, c)
-  H = tape.jacobian(h_current, x, experimental_use_pfor=False)
+  H = tape.jacobian(target = h_current, sources = x, parallel_iterations=500 , experimental_use_pfor=True)
   return h_current, H
-
-def get_h_H_sequential(self, c): 
-  with tf.GradientTape(persistent=True) as tape:
-    tape.watch(self.x)
-    h_current = h(self.x, c)
-  for i in range(s):
-    for j in range(r): 
-      self.H[r*i + j, :].assign(tape.gradient(h_current[r*i + j], self.x))
-  return h_current
 
 
 def get_censoring(Y):
@@ -83,34 +81,35 @@ def get_censoring(Y):
   return tf.keras.activations.relu(censoring)
 
 class Kalman_model(tf.Module):
-  def __init__(self, x_0, Sigma):
+  def __init__(self, x_0, sigma2):
     #super().__init__(**kwargs)
     self.x = tf.Variable(x_0)
-    self.V = tf.Variable(Sigma)
+    self.V = tf.Variable(sigma2)
     #self.H = tf.Variable(tf.zeros((s*r, n_nodes*d)))
 
   def __call__(self, y, c, censoring):
-    self.V.assign_add(Sigma)
+    self.V.assign_add(sigma2)
     mu, H = get_h_H(self.x, c)
-    #mu= get_h_H_sequential(self, c)
-    R_inv = tf.linalg.diag(1/mu)
+    # X_b =  tf.reshape(self.x, [b, n_nodes*d]) + tf.sqrt(sigma2) * tf.random.normal(shape = [b, n_nodes*d], dtype = tf.float32)
+    # Y_b = h_b()
+    R_inv = 1/mu
     if(censoring != None):
-      censoring = tf.reshape(censoring, [-1])
+      censoring = tf.squeeze(censoring)
       H = tf.matmul(tf.linalg.diag(censoring), H)
-      R_inv = tf.linalg.diag(1/mu * censoring)
+      R_inv = 1/mu * censoring
       mu = mu * censoring
-    K = tf.matmul(tf.linalg.inv(tf.linalg.inv(self.V) + tf.matmul(H, tf.matmul(R_inv, H), transpose_a=True)), tf.matmul(H, R_inv, transpose_a=True))
-    self.x.assign_add( tf.reshape(tf.matmul(K, tf.reshape(y - mu, (-1,1))), [-1]))
-    V_tmp = tf.matmul(tf.eye(n_nodes * d, dtype=tf.float32) - tf.matmul(K, H), self.V)
-    self.V.assign( (V_tmp + tf.transpose(V_tmp))/2 )
+    S_chol = tf.linalg.cholesky(tf.linalg.diag(1/self.V) + tf.matmul(H, tf.transpose(R_inv * tf.transpose(H)), transpose_a=True))
+    K = tf.linalg.cholesky_solve(S_chol, R_inv * tf.transpose(H))
+    #K = tf.matmul(tf.linalg.inv(tf.linalg.diag(1/self.V) + tf.matmul(H, tf.matmul(R_inv, H), transpose_a=True)), tf.matmul(H, R_inv, transpose_a=True))
+    self.x.assign_add( tf.squeeze(tf.matmul(K, tf.reshape(y - mu, (-1,1)))))
+    self.V.assign((tf.cast(1, dtype=tf.float32) - tf.reduce_sum(K*tf.transpose(H), axis=1)) *self.V)
     return 
 
   def smoother(self, x_prev, V_prev):
-    V_prior = V_prev + Sigma
-    B = tf.matmul(V_prev, tf.linalg.inv(V_prior))
-    self.x.assign(x_prev + tf.reshape(tf.matmul(B, tf.reshape(self.x - x_prev, (-1,1))), [-1]))
-    V_tmp = V_prev + tf.matmul(tf.matmul(B, self.V - V_prior), B, transpose_b=True)
-    self.V.assign( (V_tmp + tf.transpose(V_tmp))/2 )
+    V_prior = V_prev + sigma2
+    B = V_prev/V_prior
+    self.x.assign(x_prev + tf.squeeze(B * tf.squeeze(self.x - x_prev)))
+    self.V.assign( V_prev + tf.square(B)*(self.V - V_prior))
     return 
 
 #train_dataset_rev = tf.data.Dataset.from_tensor_slices(tf.reverse(Y, axis=[0]))
@@ -122,7 +121,7 @@ def train_kalman(model):
   for t, (y_batch, censor_batch) in enumerate(train_dataset):
     model(y_batch, c[t, :], censor_batch)
     X_kalman[t+1, :].assign(model.x)
-    V_kalman[t+1, :, :].assign(model.V)
+    V_kalman[t+1, :].assign(model.V)
     tf.print(t)
 
 
@@ -133,9 +132,9 @@ def train_smoother(model):
     t = n-tf.cast(t, dtype=tf.int32)
     if(t!=0):
       offset[t-1, :].assign(get_offset(model.x))
-    model.smoother(X_kalman[t-1, :], V_kalman[t-1, :, :])
+    model.smoother(X_kalman[t-1, :], V_kalman[t-1, :])
     X_kalman[t-1, :].assign(model.x)
-    V_kalman[t-1, :, :].assign(model.V)
+    V_kalman[t-1, :].assign(model.V)
     tf.print(t)
 
 @tf.function(autograph=False)
@@ -162,39 +161,27 @@ train_dataset = train_dataset.batch(batch_size=1)
 model_coefficients = alpha
 c = tf.Variable(tf.fill((n, s*r), model_coefficients), trainable=False)
 X_kalman = tf.Variable(tf.random.uniform(shape = (n+1,  n_nodes * d) , minval= -1, maxval= 1, dtype=tf.float32))
-V_kalman = tf.Variable(tf.random.uniform(shape = (n+1,  n_nodes * d, n_nodes * d) , minval= -1, maxval= 1, dtype=tf.float32))
+V_kalman = tf.Variable(tf.random.uniform(shape = (n+1,  n_nodes * d) , minval= -1, maxval= 1, dtype=tf.float32))
 offset = tf.Variable(tf.random.uniform(shape = (n, s*r ) , minval= -1, maxval= 1, dtype=tf.float32))
 Z = tf.ones((n, s*r), dtype=tf.float32)
 
 #x_0 = tf.random.uniform(shape = [n_nodes * d] , minval= -1, maxval= 1, dtype=tf.float32)
 x_0 = tf.reshape(X_true[0, :, :], -1)
-Sigma = tf.cast(tf.linalg.diag([0.01]*(n_nodes*d).numpy()), dtype=tf.float32)
+sigma2 = tf.constant( [0.01]*(n_nodes*d).numpy(), dtype=tf.float32)
 
 X_kalman[0,:].assign(x_0)
-V_kalman[0,:,:].assign(Sigma)
-
+V_kalman[0,:].assign(sigma2)
 
 # -------------------- Run model -------------------------
-# fit a censored process
-censor_data = get_censoring(Y)
-Y = Y*censor_data
 
-# train_dataset = tf.data.Dataset.from_tensor_slices((Y, censor_data))
-# train_dataset = train_dataset.batch(batch_size=1)
-
-model_coefficients = alpha
-c = tf.Variable(tf.fill((n, s*r), model_coefficients), trainable=False)
-
-
-model = Kalman_model(x_0, Sigma)
-for iter in range(10):
+model = Kalman_model(x_0, sigma2)
+#tf.config.run_functions_eagerly(False)
+for iter in range(2):
   train_kalman(model)
   train_smoother(model)
   model_coefficients, log_likelihood = fit_glm(tf.reshape(Z, (-1,1)), tf.reshape(Y, [-1]), tf.reshape(offset, [-1]), censor_data)
   c.assign(tf.fill((n, s*r), model_coefficients))
   tf.print("log likelihood = ", tf.reduce_sum(log_likelihood))
-
-
 
 # --------------------- Run plots ---------------------
 Y = tf.reshape(Y, (n, s*r))
@@ -203,7 +190,7 @@ for t in range( n):
    Y_est[t, :].assign(h(X_kalman[t+1, :], c[t, :]))
 
 
-n_plots = 3
+n_plots = 10
 i = tf.random.uniform(shape = [n_plots], maxval = s, dtype = tf.int32)
 j = tf.random.uniform(shape = [n_plots], maxval = r, dtype = tf.int32)
 fig, axs = plt.subplots(n_plots, n_plots, figsize=(25,25))
@@ -211,13 +198,10 @@ for i in range(n_plots):
   for j in range(n_plots):
     axs[ i, j].plot( Y[:, i + j*r ])
     axs[ i, j].plot( Y_est[:, i + j*r ])
-# plt.show()
+
 plt.savefig("model_result.png", dpi=75)
 
-
-#TODO save X_kalman and Y_est
 # Save results
-
 name = 'result_' + str(s) + '_' + str(r)
 path = '../data/results' + name
 np.save(path, X_kalman)
